83b211ae2f0e5b2ee234631cb1c66ef930710d40
Jhoan Valle <ffjhoan9@gmail.com>
2026-07-10 14:16:00 -0500

    .

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,14 +5,12 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash; // <--- Importa esto
 
 class DatabaseSeeder extends Seeder
 {
     use WithoutModelEvents;
 
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
         User::truncate();
@@ -20,15 +18,16 @@ class DatabaseSeeder extends Seeder
         User::create([
             'name' => 'Administrador Inventario',
             'email' => 'admin.nsp.2026@gmail.com',
-            'password' => '12345678',
+            'password' => Hash::make('12345678'), // <--- Usa Hash::make
             'rol' => 'Administrador',
             'email_verified_at' => now(),
         ]);
 
+        // Repite lo mismo para los otros usuarios...
         User::create([
             'name' => 'Mantenimiento',
             'email' => 'ffjhoan9@gmail.com',
-            'password' => '12345678',
+            'password' => Hash::make('12345678'),
             'rol' => 'Mantenimiento',
             'email_verified_at' => now(),
         ]);
@@ -36,7 +35,7 @@ class DatabaseSeeder extends Seeder
         User::create([
             'name' => 'Directiva',
             'email' => 'vallerobledojairjhoan@gmail.com',
-            'password' => '12345678',
+            'password' => Hash::make('12345678'),
             'rol' => 'Directiva',
             'email_verified_at' => now(),
         ]);
@@ -44,7 +43,7 @@ class DatabaseSeeder extends Seeder
         User::create([
             'name' => 'Jhoan Valle',
             'email' => 'vallejhoan602@gmail.com',
-            'password' => '12345678',
+            'password' => Hash::make('12345678'),
             'rol' => 'Administrador',
             'email_verified_at' => now(),
         ]);
